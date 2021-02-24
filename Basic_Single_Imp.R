6a2994d6e0c45b4bec3bb250db3d2037b14444bd
alexmorgan1995 <a.morgan@outlook.com>
2021-02-24 16:42:17 +0000

    Tidying and updating Chapter 3
    Have removed all of the old analyses from and added new analyses

---
+++ Basic_Single_Imp.R
@@ -0,0 +1,53 @@
+library("deSolve"); library("ggplot2"); library("plotly"); library("reshape2")
+library("bayestestR"); library("tmvtnorm"); library("ggpubr")
+
+rm(list=ls())
+setwd("C:/Users/amorg/Documents/PhD/Chapter_2/Chapter2_Fit_Data/FinalData")
+
+#Function to remove negative prevalence values and round large DP numbers
+rounding <- function(x) {
+  if(as.numeric(x) < 1e-10) {x <- 0
+  } else{signif(as.numeric(x), digits = 6)}
+}
+
+# Single Model ------------------------------------------------------------
+
+amrimp <- function(t, y, parms) {
+  with(as.list(c(y, parms)), {
+    dSa = ua + ra*(Isa + Ira) + theta*tau*Isa - (betaAA*Isa*Sa) - (betaAH*Ish*Sa) - (1-alpha)*(betaAH*Irh*Sa) - (1-alpha)*(betaAA*Ira*Sa) - ua*Sa -
+      zeta*Sa*(1-alpha) - zeta*Sa 
+    dIsa = betaAA*Isa*Sa + betaAH*Ish*Sa + phi*Ira - theta*tau*Isa - tau*Isa - ra*Isa - ua*Isa + zeta*Sa
+    dIra = (1-alpha)*betaAH*Irh*Sa + (1-alpha)*betaAA*Ira*Sa + tau*Isa - phi*Ira - ra*Ira - ua*Ira + zeta*Sa*(1-alpha)
+    
+    dSh = uh + rh*(Ish+Irh) - (betaHH*Ish*Sh) - (1-alpha)*(betaHH*Irh*Sh) - (betaHA*Isa*Sh) - (1-alpha)*(betaHA*Ira*Sh) - (betaimp*fracimp*Sh) - uh*Sh 
+    dIsh = betaHH*Ish*Sh + betaHA*Isa*Sh + (betaimp*(fracimp*(1-propres_imp))*Sh) - rh*Ish - uh*Ish 
+    dIrh = (1-alpha)*(betaHH*Irh*Sh) + (1-alpha)*(betaHA*Ira*Sh) + (betaimp*(fracimp*propres_imp)*Sh) - rh*Irh - uh*Irh 
+    return(list(c(dSa,dIsa,dIra,dSh,dIsh,dIrh)))
+  })
+}
+
+
+# Running the Basic Model -------------------------------------------------
+
+init <- c(Sa=0.98, Isa=0.01, Ira=0.01, Sh=1, Ish=0, Irh=0)
+output1 <- data.frame()
+times <- seq(0, 200, by = 1)
+
+for (i in 1:2) {
+  parms2 = c(ra = 60^-1, rh =  (5.5^-1), ua = 240^-1, uh = 28835^-1, betaAA = 0.029, betaAH = 0.00001, betaHH = 0.00001, 
+             betaHA = (0.00001), phi = 0.0131, theta = 1.13, alpha = 0.43, tau = c(0,0.0123)[i], zeta = 0.0497,
+             betaimp = 0.00001,
+             fracimp = 0.5,
+             propres_imp = 0.5)
+  
+  out <- ode(y = init, func = amrsingle, times = times, parms = parms2)
+  temp <- c(parms2[["tau"]],
+            rounding(out[nrow(out),6]) + rounding(out[nrow(out),7]),
+            rounding(out[nrow(out),7]) / (rounding(out[nrow(out),6]) + rounding(out[nrow(out),7])))
+  output1 <- rbind.data.frame(output1, temp)
+}
+
+fin_base <- c(abs(output1[1,2] - output1[2,2]),
+              abs(output1[1,3] - output1[2,3]))
+
+
