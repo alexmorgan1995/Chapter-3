c22d3e5362944aadc53ec401d48f1923fc1d2cbf
alexmorgan1995 <a.morgan@outlook.com>
2021-03-11 16:20:44 +0000

    Update Basic_Single_Imp.R

--- Basic_Single_Imp.R
+++ Basic_Single_Imp.R
@@ -14,14 +14,14 @@ rounding <- function(x) {
 
 amrimp <- function(t, y, parms) {
   with(as.list(c(y, parms)), {
-    dSa = ua + ra*(Isa + Ira) + theta*tau*Isa - (betaAA*Isa*Sa) - (betaAH*Ish*Sa) - (1-alpha)*(betaAH*Irh*Sa) - (1-alpha)*(betaAA*Ira*Sa) - ua*Sa -
+    dSa = ua + ra*(Isa + Ira) + theta*tau*Isa - (betaAA*Isa*Sa) - (1-alpha)*(betaAA*Ira*Sa) - ua*Sa -
       zeta*Sa*(1-alpha) - zeta*Sa 
-    dIsa = betaAA*Isa*Sa + betaAH*Ish*Sa + phi*Ira - theta*tau*Isa - tau*Isa - ra*Isa - ua*Isa + zeta*Sa
-    dIra = (1-alpha)*betaAH*Irh*Sa + (1-alpha)*betaAA*Ira*Sa + tau*Isa - phi*Ira - ra*Ira - ua*Ira + zeta*Sa*(1-alpha)
+    dIsa = betaAA*Isa*Sa + phi*Ira - theta*tau*Isa - tau*Isa - ra*Isa - ua*Isa + zeta*Sa
+    dIra = (1-alpha)*betaAA*Ira*Sa + tau*Isa - phi*Ira - ra*Ira - ua*Ira + zeta*Sa*(1-alpha)
     
-    dSh = uh + rh*(Ish+Irh) - (betaHH*Ish*Sh) - (1-alpha)*(betaHH*Irh*Sh) - (betaHA*Isa*Sh) - (1-alpha)*(betaHA*Ira*Sh) - (1-usage_dom)*(betaimp*fracimp*Sh) - uh*Sh 
-    dIsh = betaHH*Ish*Sh + betaHA*Isa*Sh + (betaimp*(fracimp*(1-propres_imp))*Sh) - rh*Ish - uh*Ish 
-    dIrh = (1-alpha)*(betaHH*Irh*Sh) + (1-alpha)*(betaHA*Ira*Sh) + (betaimp*(fracimp*propres_imp)*Sh) - rh*Irh - uh*Irh 
+    dSh = uh + rh*(Ish+Irh) - (betaHH*Ish*Sh) - (1-alpha)*(betaHH*Irh*Sh) - (betaHA*Isa*Sh) - (1-alpha)*(betaHA*Ira*Sh) - (1-usage_dom)*(betaHA*fracimp*Sh) - uh*Sh 
+    dIsh = betaHH*Ish*Sh + betaHA*Isa*Sh + (betaHA*(betaHA*(1-propres_imp))*Sh) - rh*Ish - uh*Ish 
+    dIrh = (1-alpha)*(betaHH*Irh*Sh) + (1-alpha)*(betaHA*Ira*Sh) + (betaHA*(betaHA*propres_imp)*Sh) - rh*Irh - uh*Irh 
     return(list(c(dSa,dIsa,dIra,dSh,dIsh,dIrh)))
   })
 }
@@ -43,7 +43,6 @@ for (i in 1:length(tauseq)) {
   parms2 = c(ra = 60^-1, rh =  (5.5^-1), ua = 240^-1, uh = 28835^-1, betaAA = 0.029, betaAH = 0.00001, betaHH = 0.00001, 
              betaHA = (0.00001), phi = 0.0131, theta = 1.13, alpha = 0.43, tau = tauseq[i], zeta = 0.0497,
              usage_dom = 0.6,
-             betaimp = 0.00001,
              fracimp = 0.5,
              propres_imp = 0.5)
   
@@ -64,69 +63,110 @@ meltoutpu1 <- melt(output1, id.vars = "tau", measure.vars = c("ResH","SensH"))
 
 ggplot(meltoutpu1, aes(x = tau, y = value, fill = variable)) + geom_bar(position="stack", stat="identity")
 
-#Testing Distributions
 
+# Testing for Monotonicity  -----------------------------------------------
+
+#The aim of this section is to look at the relationship of changing each variable on delta_FBD and delta_res
+
+
+parmdetails <- rbind(data.frame("Parameter" = "fracimp", "Value" = seq(0, 1, by = 1/100)),
+                     data.frame("Parameter" = "propres_imp", "Value" = seq(0, 1, by = 1/100)),
+                     data.frame("Parameter" = "usage_dom", "Value" = seq(0, 1, by = 1/100)),
+                     
+                     data.frame("Parameter" = "betaAA", "Value" = seq(0, 0.29, by = 0.29/100)),
+                     data.frame("Parameter" = "betaHA", "Value" = seq(0, 0.0001, by = 0.0001/100)),
+                     data.frame("Parameter" = "betaHH", "Value" = seq(0, 0.0001, by = 0.0001/100)),
+                     data.frame("Parameter" = "phi", "Value" = seq(0, 0.131, by = 0.131/100)),
+                     data.frame("Parameter" = "theta", "Value" = seq(0, 11.3, by = 11.3/100)),
+                     data.frame("Parameter" = "alpha", "Value" = seq(0, 1, by = 1/100)),
+                     data.frame("Parameter" = "zeta", "Value" = seq(0, 0.497, by = 0.497/100)),
+                     data.frame("Parameter" = "rh", "Value" = seq(0.01, 0.55^-1, by = 0.55^-1/100)),
+                     data.frame("Parameter" = "ra", "Value" = seq(0, 6^-1, by = 6^-1/100)),
+                     data.frame("Parameter" = "uh", "Value" = seq(0, 2883.5^-1, by = 2883.5^-1/100)),
+                     data.frame("Parameter" = "ua", "Value" = seq(0, 24^-1, by = 24^-1/100)))
+
+times <- seq(0,30000, by = 100) 
 init <- c(Sa=0.98, Isa=0.01, Ira=0.01, Sh=1, Ish=0, Irh=0)
-times <- seq(0, 200, by = 1)
-output_fin <- data.frame(matrix(nrow = 0, ncol = 24))
-plotlist <- list()
-
-parmtau <- seq(0, 0.035, 0.001)
-
-n = 1000
+tau_range <- c(0, 0.0123)
 
 
-for(z in 1:3) {
+parms <- c(ra = 60^-1, rh = (5.5^-1), ua = 240^-1, uh = 28835^-1, betaAA = 0.029, betaAH = 0.00001, betaHH = 0.00001, 
+           betaHA = (0.00001), phi = 0.0131, theta = 1.13, alpha = 0.43, zeta = 0.0497, usage_dom = 0.6, fracimp = 0.5, propres_imp = 0.5)
+
+suppplotlist <- list()
+
+for (j in 1:length(unique(parmdetails[,1]))) { 
   
-  parms2 = c(ra = 60^-1, rh =  (5.5^-1), ua = 240^-1, uh = 28835^-1, betaAA = 0.029, betaAH = 0.00001, betaHH = 0.00001, 
-             betaHA = (0.00001), phi = 0.0131, theta = 1.13, alpha = 0.43, tau = tauseq[i], zeta = 0.0497,
-             betaimp = 0.00001,
-             fracimp = 0,
-             propres_imp = 0)
-  
-  for(i in 1:n) {
+  suppplotlist[[j]] <- local ({ 
+    output <- data.frame()
     
-    parms[grep("tau_imp", names(parms))] <- rbeta(10, 1, 4)
-    parms[grep("psi_imp", names(parms))] <- rbeta(10, 1, 1)
-    
-    parms2 <- parms
-    
-    for(k in 1:length(parms[grep("tau_imp", names(parms))])) {
-      parms2[grep("tau_imp", names(parms))][k] <- 
-        (parms[grep("tau_imp", names(parms))][k]/  sum(parms[grep("tau_imp", names(parms))])) * 
-        ((parms[["tau_dom"]] * length(parms[grep("tau_imp", names(parms))])) - parms[["tau_dom"]])
+    for (x in 1:length(parmdetails[parmdetails == as.character(unique(parmdetails[,1])[j]),2])) { #for the individual parameter values in the sequence
+      temp1 <- data.frame()
       
-      parms2[grep("psi_imp", names(parms))][k] <- 
-        (parms[grep("psi_imp", names(parms))][k]/  sum(parms[grep("psi_imp", names(parms))])) * (1-parms[["psi_dom"]])
+      for (i in 1:length(tau_range)) {
+        temp <- data.frame(matrix(nrow = 0, ncol=3))
+        parmstemp <- c(parms, tau = tau_range[i])
+        parmstemp[as.character(unique(parmdetails[,1])[j])] <- parmdetails[parmdetails == as.character(unique(parmdetails[,1])[j]), 2][x]
+        out <- ode(y = init, func = amrimp, times = times, parms = parmstemp)
+        temp[1,1] <- (rounding(out[nrow(out),6]) + rounding(out[nrow(out),7]))*100000
+        temp[1,2] <- rounding(out[nrow(out),7])/(rounding(out[nrow(out),6]) + rounding(out[nrow(out),7]))
+        temp[1,3] <- as.character(parmdetails[parmdetails == as.character(unique(parmdetails[,1])[j]), 2][x]) #what is the parameter value used
+        temp[1,4] <- as.character(unique(parmdetails[,1])[j]) # what is the parameter explored 
+        temp1 <- rbind.data.frame(temp1, temp)
+      }
+      
+      output <- rbind(output, stringsAsFactors = FALSE,
+                      c(as.numeric(temp1[1,1]), #FBD at tau = 0
+                        as.numeric(temp1[2,1]), #FBD at tau = 0.0123
+                        as.numeric(temp1[1,2]), #ResH at tau = 0
+                        as.numeric(temp1[2,2]), #ResH at tau = 0.0123 
+                        as.numeric(temp1[1,1] - temp1[2,1]), #delta_FBD
+                        as.numeric(temp1[1,2] - temp1[2,2]), #delta_Res
+                        as.numeric(temp1[i,3]), #Parm Value
+                        as.factor(temp1[i,4]))) #Parm Name 
+      
+      print(paste0("Parameter ",unique(parmdetails[,1])[j], " | ", round(x/101, digits = 2)*100,"%" ))
     }
     
-    output1 <- data.frame(matrix(nrow = 0, ncol = 2))
+    colnames(output)[1:7] <- c("FBD_H_0", "FBD_H_123","Res_H_0", "Res_H_123", "delta_FBD", "delta_Res", "ParmValue", "Parm")
     
-    for (j in 1:2) {
-      parms2[["tau_dom"]] <- c(0, parms[["tau_dom"]])[j]
-      out <- ode(y = init, func = amrmulti, times = times, parms = parms2)
-      temp <- c(rounding(out[nrow(out),36]) + rounding(out[nrow(out),37])*100000,
-                rounding(out[nrow(out),37]) / (rounding(out[nrow(out),36]) + rounding(out[nrow(out),37])))
-      output1[j,] <- temp
-    }
+    output <- output[!is.nan(output$PercInc) & !is.nan(output$RelInc) & !is.infinite(output$PercInc),] # Remove 0s and NAs - models which don't lift off 
     
-    fin <- c(parms2[grep("tau", names(parms))], parms2[grep("psi", names(parms))],
-             abs(output1[1,1] - output1[2,1]),
-             abs(output1[1,2] - output1[2,2]))
+    plotnames <- c(bquote(Imp["FBD"]~Parameter), bquote(Imp["Res"]~Parameter), bquote(psi["Dom"]~Parameter),
+                   bquote(beta["AA"]~Parameter), bquote(beta["HA"]~Parameter), bquote(beta["HH"]~Parameter), bquote(beta["AH"]~Parameter), 
+                   bquote(phi~Parameter), bquote(theta~Parameter), bquote(alpha~Parameter), bquote(zeta~Parameter), bquote(r["H"]~Parameter), bquote(r["A"]~Parameter), 
+                   bquote(mu["H"]~Parameter), bquote(mu["A"]~Parameter))[[j]]
     
-    output_fin[i,] <- fin
+    p1 <- ggplot(output, aes(x = as.numeric(ParmValue), y = as.numeric(delta_FBD))) + theme_bw() + geom_line(lwd = 1.02, col ="darkblue") +
+      scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(limits = c(0, max(output$delta_FBD)*1.1), expand = c(0, 0)) +
+      labs(x = plotnames) + theme(plot.margin=unit(c(0.3,0.3,0.3,0.3),"cm"), axis.title.y=element_blank())
     
-    print(paste0((i/n)*100, " - psi_dom = ", parms2[["psi_dom"]]))
-  }
-  
-  colnames(output_fin) <- c(names(parms2[grep("tau", names(parms))]), names(parms2[grep("psi", names(parms))]),
-                            "diff_food", "diff_resis")
-  
-  p1 <- ggplot(data = output_fin, aes(x = diff_food, y = diff_resis)) + geom_point() + theme_bw() + 
-    geom_point(x = fin_base[1]*100000, y = fin_base[2], size = 2, col = "red") + scale_x_continuous(limits = c(0,1.5)) + scale_y_continuous(limits = c(0,1)) +
-    theme(legend.text=element_text(size=14),  axis.text=element_text(size=14),
-          axis.title.y=element_text(size=14),axis.title.x= element_text(size=14), plot.margin = unit(c(0.5,0.5,0.5,0.5), "cm"))
-  
-  plotlist[[z]] <- list(p1, output_fin)
+    p2 <- ggplot(output, aes(x = as.numeric(ParmValue), y = as.numeric(delta_Res))) + theme_bw() + geom_line(lwd = 1.02, col ="darkblue") +
+      scale_x_continuous(expand = c(0, 0)) + scale_y_continuous(limits = c(0, max(output$delta_Res)*1.1), expand = c(0, 0)) +
+      labs(x = plotnames) + theme(plot.margin=unit(c(0.3,0.3,0.3,0.3),"cm"), axis.title.y=element_blank())
+    
+    #if(unique(parmdetails[,1])[j] == "betaHA"){
+    #  p2 <- p2 + geom_vline(xintercept = 8.0e-06, col = "red", size  = 0.7, lty = 3)}
+    #if(unique(parmdetails[,1])[j] == "zeta"){
+    #  p2 <- p2 + geom_vline(xintercept = 0.011084193, col = "red", size  = 0.7, lty = 3)}
+    #if(unique(parmdetails[,1])[j] == "ra"){
+    #  p2 <- p2 + geom_vline(xintercept = 0.038333333, col = "red", size  = 0.7, lty = 3)}
+    #if(unique(parmdetails[,1])[j] == "ua"){
+    #  p2 <- p2 + geom_vline(xintercept = 0.0254166667, col = "red", size  = 0.7, lty = 3)}
+    #if(unique(parmdetails[,1])[j] == "rh"){
+     # p2 <- p2 + geom_vline(xintercept = 0.22818182, col = "red", size  = 0.7, lty = 3)}
+    
+    return(list(p1,p2))
+  })
 }
+
+#Absolute Diff
+pabdiff <- plot_grid(plot_grid(suppplotlist[[1]][[1]], suppplotlist[[2]][[1]], suppplotlist[[3]][[1]],suppplotlist[[4]][[1]], suppplotlist[[5]][[1]], 
+                               suppplotlist[[6]][[1]], suppplotlist[[7]][[1]], suppplotlist[[8]][[1]], suppplotlist[[9]][[1]], suppplotlist[[10]][[1]], suppplotlist[[11]][[1]],
+                               suppplotlist[[12]][[1]], nrow = 4, ncol =3), scale=0.95) + 
+  draw_label("% Change in ICombH Relative to Baseline Usage", x=  0, y=0.5, vjust= 1.5, angle=90, size = 12)
+
+ggsave(pabdiff, filename = "Sensitivity_RelInc.png", dpi = 300, type = "cairo", width = 5, height = 7, units = "in",
+       path = "C:/Users/amorg/Documents/PhD/Chapter_2/Figures/Redraft_v1")
+
 
