9f53c6f22b578c5be2289e1d86ffca2cbd86d264
alexmorgan1995 <48563535+alexmorgan1995@users.noreply.github.com>
2021-11-04 12:39:52 +0000

    Re-arranging files
    Have added new data files and rearranged the existing model R files

--- Part_1_Basic/AmpPigs_Simple_Mod.R
+++ Part_1_Basic/AmpPigs_Simple_Mod.R
@@ -2,22 +2,17 @@ library("deSolve"); library("ggplot2"); library("plotly"); library("reshape2"); 
 library("bayestestR"); library("tmvtnorm"); library("ggpubr"); library("cowplot"); library("lhs")
 
 rm(list=ls())
-setwd("//csce.datastore.ed.ac.uk/csce/biology/users/s1678248/PhD/Chapter_3/Data/fit_data")
-
-#Function to remove negative prevalence values and round large DP numbers
-rounding <- function(x) {
-  if(as.numeric(x) < 1e-10) {x <- 0
-  } else{signif(as.numeric(x), digits = 6)}
-}
+setwd("//csce.datastore.ed.ac.uk/csce/biology/users/s1678248/PhD/Chapter_3/Models/Chapter-3/Model_Fit_Data")
 
 # Single Model ------------------------------------------------------------
 
 amrimp <- function(t, y, parms) {
   with(as.list(c(y, parms)), {
+    
     dSa = ua + ra*(Isa + Ira) + kappa*tau*Isa - (betaAA*Isa*Sa) - (1-alpha)*(betaAA*Ira*Sa) - ua*Sa -
-      zeta*Sa*(1-alpha) - zeta*Sa 
-    dIsa = betaAA*Isa*Sa + phi*Ira - kappa*tau*Isa - tau*Isa - ra*Isa - ua*Isa + zeta*Sa
-    dIra = (1-alpha)*betaAA*Ira*Sa + tau*Isa - phi*Ira - ra*Ira - ua*Ira + zeta*Sa*(1-alpha)
+      0.5*zeta*Sa*(1-alpha) - 0.5*zeta*Sa 
+    dIsa = betaAA*Isa*Sa + phi*Ira - kappa*tau*Isa - tau*Isa - ra*Isa - ua*Isa + 0.5*zeta*Sa
+    dIra = (1-alpha)*betaAA*Ira*Sa + tau*Isa - phi*Ira - ra*Ira - ua*Ira + 0.5*zeta*Sa*(1-alpha)
     
     dSh = uh + rh*(Ish+Irh) - (betaHH*Ish*Sh) - (1-alpha)*(betaHH*Irh*Sh) - 
       psi*(betaHD*Isa*Sh) - 
@@ -36,15 +31,69 @@ amrimp <- function(t, y, parms) {
 }
 
 
+#### Data Import ####
+
+#Import Data
+dataamp_pigs_raw <- read.csv("Amp_FatPigs_Comb.csv")
+dataamp_hum_raw <- read.csv("Hum_FatPigs.csv")
+
+dataamp_pigs <- dataamp_pigs_raw
+dataamp_hum <- dataamp_hum_raw
+
+#Cleaning Data - Animals
+dataamp_pigs[,(2+5):(6+5)][dataamp_pigs[,2:6] < 10] <- NA #If N > 10, replace the particular country/year with NA for the No. of pos isolates
+dataamp_pigs[,(2+10):(6+10)][dataamp_pigs[,2:6] < 10] <- NA #If N > 10, replace the particular country/year with NA for the prop of resistant isolates
+dataamp_pigs[,2:6][dataamp_pigs[,2:6] < 10] <- NA #If N > 10, replace the particular country/year with NA for N
+dataamp_pigs <- dataamp_pigs[!(is.na(dataamp_pigs$N_2015) & is.na(dataamp_pigs$N_2016) & is.na(dataamp_pigs$N_2017) & 
+                                 is.na(dataamp_pigs$N_2018) & is.na(dataamp_pigs$N_2019)),]
+pig_yrs <- sub("N_", "", grep("N_20",colnames(dataamp_pigs), value = TRUE)) #Find years of the EFSA and ESVAC data in the dataset
+colnames(dataamp_pigs)[12:16] <- pig_yrs
+
+#Create dataset where each row is a different observation. 
+melt_amp_pigs <- melt(dataamp_pigs, id.vars = "Country", measure.vars = pig_yrs)
+melt_amp_pigs$usage <- melt(dataamp_pigs, id.vars = "Country", measure.vars = c("scale_ampusage_2015", "scale_ampusage_2016", 
+                                                                                "scale_ampusage_2017", "scale_ampusage_2018", "scale_ampusage_2019"))[,3]
+melt_amp_pigs$N <- melt(dataamp_pigs, id.vars = "Country", measure.vars = c("N_2015", "N_2016", 
+                                                                            "N_2017", "N_2018", "N_2019"))[,3]
+melt_amp_pigs$IsolPos <- melt(dataamp_pigs, id.vars = "Country", measure.vars = c("PosIsol_2015", "PosIsol_2016", 
+                                                                                  "PosIsol_2017", "PosIsol_2018", "PosIsol_2019"))[,3]
+colnames(melt_amp_pigs)[c(2,3)] <- c("Year", "Resistance")
+
+#Cleaning Data - Humans
+#only include countries/years which are present in the resistance dataset
+dataamp_hum <- dataamp_hum[dataamp_hum$Country %in% intersect(dataamp_hum$Country, dataamp_pigs$Country),]
+colnames(dataamp_hum)[26:31] <- as.character(2014:2019)
+dataamp_hum_melt <- melt(dataamp_hum, id.vars = "Country", measure.vars = pig_yrs)
+colnames(dataamp_hum_melt)[c(2,3)] <- c("Year", "Resistance")
+
+# Combine Human and Livestock Dataset -----------------------------------------------------------------
+melt_amp_pigs$ResPropHum <- dataamp_hum_melt[,3] #Obtain the melted human resistances
+
+melt_amp_pigs <- melt_amp_pigs[!is.na(melt_amp_pigs$Resistance),]
+melt_amp_pigs <- melt_amp_pigs[!is.na(melt_amp_pigs$usage),] # Remove all rows with NAs for usage and resistance
+
+#Add 95% CIs for each datapoint
+melt_amp_pigs$lower_amp <- unlist(lapply(1:nrow(melt_amp_pigs), function(i) prop.test(melt_amp_pigs$IsolPos[i],melt_amp_pigs$N[i])[[6]][[1]]))
+melt_amp_pigs$upper_amp <- unlist(lapply(1:nrow(melt_amp_pigs), function(i) prop.test(melt_amp_pigs$IsolPos[i],melt_amp_pigs$N[i])[[6]][[2]]))
+
+#Rename the columns
+colnames(melt_amp_pigs) <- c("Country", "Year", "ResPropAnim", "Usage", "N", "IsolPos", "ResPropHum", "Lower_Amp", "Upper_Amp")
+melt_amp_pigs$Usage <- melt_amp_pigs$Usage/1000 #Change from mg/PCU to g/PCU
+
+ggplot(melt_amp_pigs, aes(x = Usage, y= ResPropAnim, color = Country)) + geom_point() +
+  scale_x_continuous(expand = c(0, 0), limits = c(0,0.055)) + scale_y_continuous(expand = c(0, 0), limits = c(0,1)) +
+  labs(x ="Livestock Antibiotic Usage (g/PCU)", y = "Antibiotic-Resistant Livestock Carriage")
+
 # Data Import -------------------------------------------------------------
 
-country_data_imp <- read.csv("//csce.datastore.ed.ac.uk/csce/biology/users/s1678248/PhD/Chapter_3/Models/Chapter-3/Model Fit Data/FullData_2021_v1_trim.csv") #This is data for pigs 
+country_data_imp <- read.csv("//csce.datastore.ed.ac.uk/csce/biology/users/s1678248/PhD/Chapter_3/Models/Chapter-3/Model_Fit_Data/FullData_2021_v1_trim.csv") #This is data for pigs 
 country_data_imp$Foodborne_Carriage_2019 <- country_data_imp$Foodborne_Carriage_2019/100
 country_data_imp$Corrected_Usage_18 <- country_data_imp$Corrected_Usage_18/100
 country_data_imp[,12:13] <- country_data_imp[,12:13]/1000
 
-country_data_gen <- read.csv("//csce.datastore.ed.ac.uk/csce/biology/users/s1678248/PhD/Chapter_3/Models/Chapter-3/Model Fit Data/res_sales_generalfit.csv") #This is data for pigs 
-country_data_gen[,13:14] <- country_data_gen[,13:14]/1000
+
+dataamp_pigs_raw
+
 
 UK_tet <- country_data_gen$scaled_sales_tet[country_data_gen$Country == "United Kingdom"]
 UK_amp <- country_data_gen$scaled_sales_amp[country_data_gen$Country == "United Kingdom"]
